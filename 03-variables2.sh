b9216fe4e42a25199a3f4186ec41c3a854b87557
PradeepGorle4 <pradeepgorle4@gmail.com>
2024-12-30 12:27:42 +0530

    This is just for practice

--- 03-variables2.sh
+++ 03-variables2.sh
@@ -2,6 +2,28 @@
 
 # This script prompts for user input
 
+echo "Please input 1st number: "
+read -s num1 # here num1 is the variable name
+
+echo "Please input 2nd number:"
+read -s num2 # here num2 is the variable name
+
+echo "Please enter the sum:"
+
+read -s sum # here sum is another variable name
+
+output=$(($num1+$num2))
+
+echo "The output is $output"
+
+if [ $sum -ne $output ]
+then 
+    echo "Output not matched. you do not know Maths. Please learn maths"
+else
+    echo "The value has not matched the output. you can proceed further with authentication"
+fi
+
+
 echo "Please enter your username:"
 
 read username
@@ -9,4 +31,6 @@ read username
 echo "please enter your password:"
 
 read -s password
+
+echo "you are successfully authenticated"
 
