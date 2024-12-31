d03fcad33d5d17b6ae4a35079ea99a319537382e
PradeepGorle4 <pradeepgorle4@gmail.com>
2024-12-31 16:28:10 +0530

    This is just for practice

--- 03-variables2.sh
+++ 03-variables2.sh
@@ -23,7 +23,7 @@ echo "The output is $output"
 if [ $sum -ne $output ]
 then 
     echo " ERROR: Output not matched. you do not know Maths. Please learn maths"
-    exit 1
+    exit 1 # This is the exit status, you can give anything other than 0 if you want to exit the script
 else
     echo "The value has not matched the output. you can proceed further with authentication"
 fi
---
+++ 05-special_variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "This script is for learning special variables"
+
