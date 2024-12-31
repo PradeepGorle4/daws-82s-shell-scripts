ad7d79bc9c9e247e4a4a2c9408c8f15f312aff0f
PradeepGorle4 <pradeepgorle4@gmail.com>
2024-12-31 16:37:07 +0530

    This is just for practice

--- 05-special_variables.sh
+++ 05-special_variables.sh
@@ -2,3 +2,14 @@
 
 echo "This script is for learning special variables"
 
+echo "All variables passed:: $@"
+echo "Number of variables passed:: $#"
+echo "script_name:: $0 "
+echo "Present working directory:: $PWD"
+echo "Home directory of current user:: $HOME"
+echo "which user is running the script:: $USER"
+
+echo "Process id of the current script:: $$"
+
+sleep 30 &
+echo "Process id of the last executed background process:: $!"
