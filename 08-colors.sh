414cd1af5a52fedf8bbe32842f1339b824e6f0bd
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-01-02 11:37:06 +0530

    This is just for practice

--- 08-colors.sh
+++ 08-colors.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-TIMESTAMP=$(date +%Y-%m-%d)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M)
 
 # Assigning variables to color codes
 
@@ -10,6 +10,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript.logs"
+SCRIPT_NAME=&(echo $0 | cut -d "." f1)
+LOG_FILE_Name="$LOGS_FOLDER-$SCRIPT_NAME-$TIMESTAMP"
 
 if [ $USERID -ne 0 ]
 then
@@ -30,18 +34,17 @@ pre_check (){
     if [ $? -ne 0 ]
     then
         echo -e " $Y Installing.....$1 $N "
-        dnf install $1 -y
+        dnf install $1 -y &>> $LOG_FILE_Name
         validate $1
     else
         echo -e " $Y $1 is already installed $N "
     fi
 }
 
-dnf list installed git
+dnf list installed git &>> $LOG_FILE_Name
 
 pre_check git
 
-dnf list installed mysql
+dnf list installed mysql &>> $LOG_FILE_Name
 
-pre_check mysql
-    
+pre_check mysql  
