6f79d0b7ec97d4014ed9ba09e7044955d811241d
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-01-04 08:12:11 +0530

    This is just for practice

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -2,23 +2,31 @@
 
 userid=$(id -u)
 
+# colors
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
 if [ $userid -ne 0 ]
 then
-    echo "ERROR: you are not root user"
+    echo -e " $R ERROR: you are not root user $N"
     exit 1
 fi
 
 validate () {
     if [ $? -eq 0 ]
     then
-        echo "Git is already installed"
+        echo -e " $Y $1 is already installed $N"
     else
         dnf install $1 -y
         if [ $? -eq 0 ]
         then
-            echo "Installing $1 ........SUCCESS"
+            echo -e "Installing $1 ........ $G SUCCESS $N"
         else
-            echo "Installing $1 ........FAILURE"
+            echo -e "Installing $1 ........$R FAILURE $N"
         fi
     fi
 }
