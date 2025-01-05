7f4270e4914227842deb576c40a515a67eab9b99
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-01-05 14:38:25 +0530

    Deleting old logs

---
+++ 15-top-word-counter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# colors
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M)
+
+while -r read line
+do
+ for word in $line
+	echo "word = $word"
+done < 04-arrays.sh
