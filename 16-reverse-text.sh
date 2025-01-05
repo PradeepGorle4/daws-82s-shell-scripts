169f0f5b16f3298458d059c18030c7366879db9f
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-01-05 16:22:54 +0530

    Deleting old logs

--- 16-reverse-text.sh
+++ 16-reverse-text.sh
@@ -8,10 +8,20 @@ N="\e[0m"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M)
 
 # Q - Write a script to reverse the content of a text file line by line and save it to a new file.
+validate() {
+    if [ $? -ne 0 ]
+    do
+        echo -e "$1........$R FAILED $N"
+        echo -e "$1........$G SUCCESS $N"
+}
+
+source_dir="/home/ec2-user/daws-82s-shell-scripts"
+source_file=$source_dir/normal.txt
 
 while read -r line
 do
 	echo "$line" | rev >> reverse.txt # rev cmd reverse the content of each line and appends to the txt file here
-done < abc.txt
+    validate "reversing the content"
+done < $source_file
 
 
